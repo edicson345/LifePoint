06a816fce23c68509bae8f2a74dff708a6a94370
edicson345 <edicson345@gmail.com>
2017-02-05 21:13:41 -0500

    added qr reader

--- app/src/main/java/com/example/ed/lifepoint/MainActivity.java
+++ app/src/main/java/com/example/ed/lifepoint/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.ed.lifepoint;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity implements ZXingScannerView.ResultHandler{
+    private ZXingScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View v)
+    {
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    public void handleResult(Result result)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Welcome Caregiver:");
+        builder.setMessage(result.getText());
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        //mScannerView.resumeCameraPreview(this);
     }
 }
