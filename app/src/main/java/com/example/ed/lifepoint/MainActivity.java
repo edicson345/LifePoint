f34361659bbb681147d5d6e5699f28883c924496
edicson345 <edicson345@gmail.com>
2017-03-13 14:27:58 -0400

    splash screen

--- app/src/main/java/com/example/ed/lifepoint/MainActivity.java
+++ app/src/main/java/com/example/ed/lifepoint/MainActivity.java
@@ -2,24 +2,34 @@ package com.example.ed.lifepoint;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.google.zxing.Result;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 
-public class MainActivity extends Activity implements ZXingScannerView.ResultHandler{
+public class MainActivity extends Activity implements ZXingScannerView.ResultHandler, View.OnClickListener {
     private ZXingScannerView mScannerView;
+    Button btn;
+    private TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.button1);
+        txt = (TextView)findViewById(R.id.textView1);
 
     }
 
     public void onClick(View v)
     {
+        txt = (TextView)findViewById(R.id.textView1);
+        txt.setText("It has changed.");
         mScannerView = new ZXingScannerView(this);
         setContentView(mScannerView);
         mScannerView.setResultHandler(this);
@@ -35,10 +45,28 @@ public class MainActivity extends Activity implements ZXingScannerView.ResultHan
     public void handleResult(Result result)
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Welcome Caregiver:");
-        builder.setMessage(result.getText());
-        AlertDialog dialog = builder.create();
-        dialog.show();
+
+        if(result.getText().equals("Nurse Joy"))
+        {
+            builder.setTitle("Welcome Caregiver:");
+            builder.setMessage(result.getText());
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            builder.setTitle("Incorrect ID");
+            builder.setMessage(result.getText());
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
 
         //mScannerView.resumeCameraPreview(this);
     }
--- app/src/main/java/com/example/ed/lifepoint/SplashActivity.java
+++ app/src/main/java/com/example/ed/lifepoint/SplashActivity.java
@@ -1,23 +1,17 @@
 package com.example.ed.lifepoint;
 
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 
-public class SecondActivity extends Activity implements View.OnClickListener
+public class SplashActivity extends AppCompatActivity
 {
-
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_activity);
-    }
 
-    public void onClick(View v)
-    {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 }
